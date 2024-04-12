8b75be80afbd50faa2e1dd98891b79736b5c6284
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-12 21:13:39 +0300

    Update Ex26.03.2024 - Vehicle.js

--- Ex26.03.2024 - Vehicle.js
+++ Ex26.03.2024 - Vehicle.js
@@ -3,12 +3,10 @@ class Vehicle {
         this.type = type;
         this.model = model;
         this.parts = parts;
-        
         this.parts.quality = this.parts.engine*this.parts.power;
         this.fuel = fuel;
     }
     drive(fuelLoss) {
         this.fuel -= fuelLoss;
     }
-
 }
